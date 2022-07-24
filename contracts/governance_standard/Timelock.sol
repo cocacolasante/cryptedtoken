f210479d11620d4860c4ca44514f379b4bb7df5f
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2022-07-23 22:58:39 -0400

    timelock smart contract completed

--- contracts/governance_standard/Timelock.sol
+++ contracts/governance_standard/Timelock.sol
@@ -4,7 +4,11 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/governance/TimelockController.sol";
 
 contract Timelock is TimelockController{
-    constructor(){
+    constructor(
+        uint256 minDelay,
+        address[] memory proposers,
+        address[] memory executors
+    )TimelockController(minDelay, proposers, executors){
         
     }
 
